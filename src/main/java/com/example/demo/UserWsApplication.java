f23e34c17602d9b27d76fa475e045f9361f3972e
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-01 09:45:28 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/UserWsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserWsApplication.class, args);
+	}
+
+}
